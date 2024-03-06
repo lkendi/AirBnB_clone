123d8c22e2fac162bb098e92a299fa53f169e655
Ridan Hawi <ridanhawy@gmail.com>
2024-03-06 23:12:47 +0300

    creating BaseModel from Dict

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
 
     """
 
-    def __init__(self, created_at=None, updated_at=None):
+    def __init__(self, id=None, created_at=None, updated_at=None, *args, **kwargs):
         """Constructor - initializes a new instance of the BaseModel class
 
         Args:
@@ -33,9 +33,15 @@ class BaseModel:
             updated_at (date_time, optional): date and time when the instance
                     was last updated. Default value of current date and time
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+                if key == "created_at" or "updated_at":
+                    setattr(self, key, datetime.strftime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = created_at or datetime.now()
+            self.updated_at = updated_at or datetime.now()
 
     def __str__(self):
         """Returs a string representation of an instance"""
