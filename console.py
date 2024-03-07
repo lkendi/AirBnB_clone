65fcc361de27755f3db70a737692b6cd09c7c123
lkendi <lkendi003@gmail.com>
2024-03-07 22:23:13 +0300

    Edit console.py

--- console.py
+++ console.py
@@ -9,17 +9,47 @@ class HBNBCommand(cmd.Cmd):
     """HBNB command interpreter class"""
     prompt = "(hbnb) "
 
-    def do_quit(self, line):
+    def do_quit(self, args):
         """Quit command - exits the program"""
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, args):
         """EOF - exists the program"""
         print()
         return True
 
-    def emptyline(self):
-        """Handles empty line + ENTER"""
+    def emptyargs(self):
+        """Handles empty args + ENTER"""
+        pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it to JSON
+        file and prints the id"""
+        # TODO: Add code
+        pass
+
+    def do_show(self, args):
+        """Prints the string representation of an inatance based on
+        the classname and id"""
+        # TODO: Add code
+        pass
+
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id
+        (save the change into the JSON file)"""
+        # TODO: Add code
+        pass
+
+    def do_all(self, args):
+        """Prints a string representation of all instances based
+        or not on the class name"""
+        # TODO: Add code
+        pass
+
+    def do_update(self, args):
+        """ Updates an instance based on the class name and id by
+        adding or updating attribute (save the change into the JSON file)"""
+        # TODO: Add code
         pass
 
 
