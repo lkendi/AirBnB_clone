556823ce54733e4db008b40e415125ba3ec141b8
Ridan Hawi <ridanhawy@gmail.com>
2024-03-08 22:01:11 +0300

    doc user.py

--- models/user.py
+++ models/user.py
@@ -2,6 +2,15 @@
 from base_model import BaseModel
 
 class User(BaseModel):
+    """
+    A class User that inherits from BaseModel
+
+    Attributes:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string 
+    """
     def __init__(self):
         super().__init__()
         self.email = "" 
