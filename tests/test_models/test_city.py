1065fa12c232b9f9819a044a56393ee8251f9266
lkendi <lkendi003@gmail.com>
2024-03-11 00:55:03 +0300

    Tests

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+    def setUp(self):
+        self.city = City()
+
+    def test_instance(self):
+        self.assertIsInstance(self.city, City)
+
+    def test_inherited_from_base_model(self):
+        self.assertTrue(issubclass(City, BaseModel))
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.city, "id"))
+        self.assertTrue(hasattr(self.city, "created_at"))
+        self.assertTrue(hasattr(self.city, "updated_at"))
+        self.assertTrue(hasattr(self.city, "state_id"))
+        self.assertTrue(hasattr(self.city, "name"))
+        self.assertIsInstance(self.city.state_id, str)
+        self.assertIsInstance(self.city.name, str)
+
+    def test_default_attribute_values(self):
+        self.assertEqual(self.city.state_id, "")
+        self.assertEqual(self.city.name, "")
+
+    def test_str(self):
+        expected_str = "[City] ({}) {}".format(
+            self.city.id, self.city.__dict__)
+        self.assertEqual(str(self.city), expected_str)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import unittest
+from models.review import Review
+from models.base_model import BaseModel
+
+
+class TestReview(unittest.TestCase):
+    def setUp(self):
+        self.review = Review()
+
+    def test_instance(self):
+        self.assertIsInstance(self.review, Review)
+
+    def test_inherited_from_base_model(self):
+        self.assertTrue(issubclass(Review, BaseModel))
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.review, "id"))
+        self.assertTrue(hasattr(self.review, "created_at"))
+        self.assertTrue(hasattr(self.review, "updated_at"))
+        self.assertTrue(hasattr(self.review, "place_id"))
+        self.assertTrue(hasattr(self.review, "user_id"))
+        self.assertTrue(hasattr(self.review, "text"))
+        self.assertIsInstance(self.review.place_id, str)
+        self.assertIsInstance(self.review.user_id, str)
+        self.assertIsInstance(self.review.text, str)
+
+    def test_default_attribute_values(self):
+        self.assertEqual(self.review.place_id, "")
+        self.assertEqual(self.review.user_id, "")
+        self.assertEqual(self.review.text, "")
+
+    def test_str(self):
+        expected_str = f"[Review] ({self.review.id}) {self.review.__dict__}"
+        self.assertEqual(str(self.review), expected_str)
+
+
+if __name__ == "__main__":
+    unittest.main()
